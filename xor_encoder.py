93ff86f16ee6b02c070306d253605582c430009c
utkuberkaytan <69167181+utkuberkaytan@users.noreply.github.com>
2025-06-02 00:22:16 +0300

    Update xor_encoder.py

--- xor_encoder.py
+++ xor_encoder.py
@@ -1,4 +1,9 @@
 import base64
+import requests
+
+url = "https://raw.githubusercontent.com/utkuberkaytan/notorious_project/refs/heads/main/keys"
+res = requests.get(url)
+lines = res.text.strip().split("\n")
 
 def xor_encrypt(data: bytes, key: bytes) -> bytes:
     return bytes([b ^ key[i % len(key)] for i, b in enumerate(data)])
@@ -7,7 +12,7 @@ with open("payload.py", "rb") as f:
     payload = f.read()
 
 b64_payload = base64.b64encode(payload)
-xor_key = b"your_key"
+xor_key = lines[0].strip().encode()
 encrypted = xor_encrypt(b64_payload, xor_key)
 
 with open("payload.bin", "wb") as f:
